daca5877bbfa2a8a24d3043e098f38156f815b95
StarlitGhost <679547+StarlitGhost@users.noreply.github.com>
2021-06-04 18:32:07 +0100

    Add Rose to aliases, fix genStats.sh input args

--- genStats.sh
+++ genStats.sh
@@ -17,10 +17,10 @@ fi
 touch "$conf_dir/stats-gen.lock"
 
 echo "### Splitting single .weechatlog into daily logs..."
-python2 $conf_dir/splitLogs.py -f $source_log -p $logs_dir/newserver.desertbus. -s .weechatlog -c
+python2 "$conf_dir/splitLogs.py" -f "$source_log" -p "$logs_dir/newserver.desertbus." -s .weechatlog -c
 
 echo "### Importing daily logs and generating stats page..."
-php -d memory_limit=1024M $sss_dir/sss.php -c $conf_dir/desertbus.conf -i $logs_dir -o $website_dir/index.html &>> $conf_dir/sss.log
+php -d memory_limit=1024M "$sss_dir/sss.php" -c "$conf_dir/desertbus.conf" -i "$logs_dir" -o "$website_dir/index.html" &>> "$conf_dir/sss.log"
 
 rm "$conf_dir/stats-gen.lock"
 
